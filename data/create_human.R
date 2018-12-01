13b4754b503bb4a1cd6d3fb7c16c313fc10b83b9
P3tt3ri <44419000+P3tt3ri@users.noreply.github.com>
2018-12-01 17:11:17 +0200

    Ex 5

--- data/create_human.R
+++ data/create_human.R
@@ -1,32 +1,54 @@
 # A short data wrangling script for the IODS course RStudio Exercise 4.
-#
-# I'm in the habit of setting the working directory excplicitly. If you need to do the same, uncomment the next line.
+# Petteri Moilanen
+# Human develompment data set
+# http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human1.txt
+
+# Description of 'human' dataset variables. 
+# Original data from: http://hdr.undp.org/en/content/human-development-index-hdi
+# Retrieved, modified and analyzed by Tuomo Nieminen 2017
+
+# The data combines several indicators from most countries in the world
+
+# "Country" = Country name
+
+# Health and knowledge
+
+# "GNI" = Gross National Income per capita
+# "Life.Exp" = Life expectancy at birth
+# "Edu.Exp" = Expected years of schooling 
+# "Mat.Mor" = Maternal mortality ratio
+# "Ado.Birth" = Adolescent birth rate
+
+# Empowerment
+
+# "Parli.F" = Percetange of female representatives in parliament
+# "Edu2.F" = Proportion of females with at least secondary education
+# "Edu2.M" = Proportion of males with at least secondary education
+# "Labo.F" = Proportion of females in the labour force
+# "Labo.M" " Proportion of males in the labour force
+
+# "Edu2.FM" = Edu2.F / Edu2.M
+# "Labo.FM" = Labo2.F / Labo2.M
+
 setwd("~/GitHub/IODS-project/data")
-# Read data
-hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
-gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
-#
-str(hd)
-summary(hd)
-str(gii)
-summary(gii)
 
+human <- read.table("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human1.txt", 
+                    sep  =",", header = T)
+
+names(human)
+str(human)
+dim(human)
+summary(human)
+library(stringr)
+human$GNI = as.numeric(str_replace(human$GNI, pattern=",", replace =""))
+keep <- c("Country", "Edu2.FM", "Labo.FM", "Life.Exp", "Edu.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F")
 library(dplyr)
-hd2 <- hd %>% rename(hdiRank=HDI.Rank,country=Country,index=Human.Development.Index..HDI.,
-                     lifeExp=Life.Expectancy.at.Birth,expYersOfEdu=Expected.Years.of.Education,
-                     meanYearsOfEdu=Mean.Years.of.Education, gni=Gross.National.Income..GNI..per.Capita,
-                     gniRankMinusHdiRank = GNI.per.Capita.Rank.Minus.HDI.Rank)
-gii2 <- gii %>% rename(giiRank=GII.Rank,country=Country,inequalityIndex=Gender.Inequality.Index..GII.,
-                       matMort=Maternal.Mortality.Ratio,adolBirth=Adolescent.Birth.Rate,
-                       parlamentRatio=Percent.Representation.in.Parliament,
-                       secondEduFem=Population.with.Secondary.Education..Female.,
-                       secondEduMale=Population.with.Secondary.Education..Male.,
-                       labourPartFem=Labour.Force.Participation.Rate..Female.,
-                       labourPartMale=Labour.Force.Participation.Rate..Male.)
-
-gii2$eduRatio = gii2$secondEduFem/gii2$secondEduMale
-gii2$labourRatio = gii2$labourPartFem/gii2$labourPartMale
-human=inner_join(hd2,gii2,by="country")
-
-write.csv2(human,"~/GitHub/IODS-project/data/human.csv",row.names=FALSE)
+human <- select(human, one_of(keep))
+# Only rows with no missing values
+human <- filter(human, complete.cases(human))
+# Exclude last 7 rows (region data)
+human <- human[1:155,]
+rownames(human) <- human$Country
+human <- human[-1]
+write.csv2(human,"~/GitHub/IODS-project/data/human.csv",row.names=TRUE)
 
